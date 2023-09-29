0e47706c2b82d8584695579ef028ad55af17e778
Stefan Klaus <stefanklaus@protonmail.com>
2023-09-30 01:49:17 +0200

    now an array

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     println!("Hello, world!");
     println!("1 + 2 = {}", 1u32 + 2);
+    let pair: (u32, i32) = (12, -6);
+    println!("Pair is {:?}", pair);
+
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", xs);
 }
+
